b8fcbfcce7b9fa55dcce28bc2c9ad1aab0a66bcb
jose luis <tjl_troche@hotmail.com>
2020-07-29 22:45:09 -0300

    cambio ejercicio 006

--- grupos/grupo5/006-ejercicios/src/index.js
+++ grupos/grupo5/006-ejercicios/src/index.js
@@ -17,7 +17,7 @@ for (let i = 0; i < MOUNTAINS.length; i++) {
     document.getElementById('app').innerHTML += '<td>'+ MOUNTAINS[i].place +'</td>';
     document.getElementById('app').innerHTML = '</tr>';
 }
-document.getElementById('app').innerHTML = '</table>';**/
+document.getElementById('app').innerHTML = '</table>';
 function mostrarOcultarTablas1() {
   let mostrado = 0;
   let elem = document.getElementById('app');
@@ -76,12 +76,68 @@ function myFunction1() {
     tr.appendChild(tdSitio);
     cuerpoTabla.appendChild(tr);
   });
+}*/
+
+
+const cuerpoTabla = document.getElementById('app');
+const boton = document.getElementById('btPinchable1');
+const boton1 = document.getElementById('btPinchable2');
+
+boton.addEventListener('click', armarTablaI(boton.id));
+boton1.addEventListener('click', armarTablaI(boton1.id));
+
+function armarTablaI(id) {
+  return () => {
+    if (cuerpoTabla.children.length !== 0) {
+      cuerpoTabla.removeChild(cuerpoTabla.lastElementChild);
+    }
+    generarTabla(id);
+  };
+
 }
+
+function generarTabla(id) {
+  let table = document.createElement('table');
+  let trCabecera = document.createElement('tr');
+  let thNombre = document.createElement('th');
+  let thAltura = document.createElement('th');
+  let thSitio = document.createElement('th');
+  if (id === 'btPinchable1') {
+    thNombre.textContent = 'Name';
+    thAltura.textContent = 'Height';
+    thSitio.textContent = 'Place';
+  } else {
+    thNombre.textContent = 'Nombre';
+    thAltura.textContent = 'Altura';
+    thSitio.textContent = 'Sitio';
+  }
+  trCabecera.appendChild(thNombre);
+  trCabecera.appendChild(thAltura);
+  trCabecera.appendChild(thSitio);
+  table.appendChild(trCabecera);
+  cuerpoTabla.appendChild(table);
+  MOUNTAINS.forEach((element) => {
+    let tr = document.createElement('tr');
+    let tdNombre = document.createElement('td');
+    tdNombre.textContent = element.name;
+    tr.appendChild(tdNombre);
+    let tdAltura = document.createElement('td');
+    tdAltura.textContent = element.height;
+    tr.appendChild(tdAltura);
+    let tdSitio = document.createElement('td');
+    tdSitio.textContent = element.place;
+    tr.appendChild(tdSitio);
+    table.appendChild(tr);
+    cuerpoTabla.appendChild(table);
+  });
+}
+
+
 
 //------------------generar lista
 
 const TODO = ['Queso', 'Tomates', 'Zanahorias', 'Toalla'];
-
+/*
 const cuerpoTabla = document.getElementById('myList');
 
 TODO.forEach((element) => {
@@ -125,3 +181,76 @@ const Eliminar = function () {
   //list.removeAttribute('idLi');
   list.removeChild(list.lastElementChild);
 };
+*/
+
+
+
+const cuerpoTabla2 = document.getElementById('myList');
+
+TODO.forEach((element) => {
+  const li = document.createElement('LI');
+  li.id = 'idLi';
+  const boton = document.createElement('button');
+  boton.style.cssText= "background-color:#ee3a14;list-style:none;";
+  boton.class = 'boton_peresonalizado_tabla';
+  boton.dataset.TODO = JSON.stringify(TODO);
+  boton.textContent = 'Eliminar';
+  boton.addEventListener('click', ()=>{
+    Eliminar (li)
+  });
+  let textnode = document.createTextNode(element);
+  li.appendChild(textnode);
+  li.appendChild(boton);
+  cuerpoTabla2.appendChild(li);
+});
+
+document.getElementById('agregarLista').onclick = function () {
+  let text = document.getElementById('myText').value;
+  agregLista(text);
+};
+
+
+
+function agregLista(text) {
+  if (text===""){
+    window.alert("No se puede agregar elementos vacios.");
+  }else{
+    if (!TODO.find(element=>
+      element===text)){
+        let li = document.createElement('LI');
+  li.id = 'idLi';
+  const boton = document.createElement('button');
+  boton.class = 'boton_peresonalizado_tabla';
+  boton.style.cssText= "background-color:#ee3a14;list-style:none;";
+  boton.textContent = 'Eliminar';
+  boton.addEventListener('click', ()=>{
+    Eliminar (li)
+  });
+  let text = document.getElementById('myText').value;
+  let t = document.createTextNode(text);
+  /*TODO.push(t);
+  console.log(TODO);
+  let txt;
+  TODO.forEach(element=>{
+    txt= document.createTextNode(element===t);
+    console.log(txt);
+    li.appendChild(txt.data);
+  })*/
+ 
+  li.appendChild(t);
+ 
+  li.appendChild(boton);
+  document.getElementById('myList').appendChild(li);
+      }else{
+        window.alert("El elemento ya existe en la lista.")
+      }
+  }
+  
+}
+
+function Eliminar(elementoLista) {
+  const list = document.getElementById('myList');
+  const antiguoHijo = list.removeChild(elementoLista);
+  let idx = TODO.indexOf(elementoLista);
+  TODO.splice.idx;
+}
